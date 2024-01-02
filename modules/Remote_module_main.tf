a40dfd3301323293e50f88828da1c423540fb5ce
Daisysinthu <daisydharshini2696@gmail.com>
2024-01-02 13:29:46 +0530

    Create Remote_module_main.tf

---
+++ modules/Remote_module_main.tf
@@ -0,0 +1,6 @@
+
+// Go to terraform modules page and search for available modules Eg:
+module "virtual-machine_example_basic" {
+  source  = "Azure/virtual-machine/azurerm//examples/basic"
+  version = "1.1.0"
+}
